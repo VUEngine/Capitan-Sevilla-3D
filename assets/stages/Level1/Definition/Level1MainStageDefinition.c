11bf33256ca9d3b4061a289d535cc0dc1662b3ff
Jorge Eremiev <jorgech3@gmail.com>
2018-04-12 20:55:08 -0500

    Tweaked projection values.

--- assets/stages/Level1/Definition/Level1MainStageDefinition.c
+++ assets/stages/Level1/Definition/Level1MainStageDefinition.c
@@ -384,7 +384,7 @@ StageROMDef LEVEL_1_MAIN_STAGE_ST =
 		// optical configuration values
 		{
 			// maximum x view distance's power into the horizon (x, y)
-			2048/2, 4096,
+			__MAXIMUM_X_VIEW_DISTANCE, __MAXIMUM_Y_VIEW_DISTANCE,
 			// distance of the eyes to the screen
 			__DISTANCE_EYE_SCREEN,
 			// distance from left to right eye (depth sensation)
