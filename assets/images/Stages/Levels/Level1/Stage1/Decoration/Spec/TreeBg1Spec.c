e0428fec17398dfc713f286f4fd2d61ee41af6a8
Christian Radke <chris@vr32.de>
2019-06-06 23:47:28 +0200

    Fixed a bunch of char specs

--- assets/images/Stages/Levels/Level1/Stage1/Decoration/Spec/TreeBg1Spec.c
+++ assets/images/Stages/Levels/Level1/Stage1/Decoration/Spec/TreeBg1Spec.c
@@ -46,7 +46,7 @@ CharSetROMSpec TREE_BG_1_CH =
 	// number of chars, depending on allocation type:
 	// __ANIMATED_SINGLE*, __ANIMATED_SHARED*: number of chars of a single animation frame (cols * rows)
 	// __ANIMATED_MULTI, __NOT_ANIMATED: sum of all chars
-	56,
+	57,
 
 	// allocation type
 	// (__ANIMATED_SINGLE, __ANIMATED_SINGLE_OPTIMIZED, __ANIMATED_SHARED, __ANIMATED_SHARED_COORDINATED, __ANIMATED_MULTI or __NOT_ANIMATED)
