a3f7d6af38d4e3e0edd638f31083cadf1d621996
Christian Radke <chris@vr32.de>
2021-04-09 22:45:33 +0200

    Removed unused GUI element

--- assets/images/Gui/Gui/Spec/GuiSpec.c
+++ assets/images/Gui/Gui/Spec/GuiSpec.c
@@ -47,7 +47,7 @@ CharSetROMSpec GUI_CH =
 	// number of chars, depending on allocation type:
 	// __ANIMATED_SINGLE*, __ANIMATED_SHARED*: number of chars of a single animation frame (cols * rows)
 	// __ANIMATED_MULTI, __NOT_ANIMATED: sum of all chars
-	70,
+	52,
 
 	// allocation type
 	// (__ANIMATED_SINGLE, __ANIMATED_SINGLE_OPTIMIZED, __ANIMATED_SHARED, __ANIMATED_SHARED_COORDINATED, __ANIMATED_MULTI or __NOT_ANIMATED)
