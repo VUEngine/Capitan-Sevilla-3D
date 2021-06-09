60ef47e1ef64632e21f8483fea212b2e09d62ce1
Christian Radke <c.radke@posteo.de>
2021-06-09 11:31:18 +0200

    Fix image palettes

--- assets/images/Stages/Credits/Single/Spec/CreditsBackersBSpec.c
+++ assets/images/Stages/Credits/Single/Spec/CreditsBackersBSpec.c
@@ -46,7 +46,7 @@ CharSetROMSpec CREDITS_BACKERS_B_CH =
 	// number of chars, depending on allocation type:
 	// __ANIMATED_SINGLE*, __ANIMATED_SHARED*: number of chars of a single animation frame (cols * rows)
 	// __ANIMATED_MULTI, __NOT_ANIMATED: sum of all chars
-	516,
+	515,
 
 	// allocation type
 	// (__ANIMATED_SINGLE, __ANIMATED_SINGLE_OPTIMIZED, __ANIMATED_SHARED, __ANIMATED_SHARED_COORDINATED, __ANIMATED_MULTI or __NOT_ANIMATED)
