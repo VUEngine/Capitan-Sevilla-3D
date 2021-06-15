5c009deff856b334d155e93202cc4492714c7016
jorgeche <jorgech3@gmail.com>
2021-06-14 19:11:39 -0500

    Restored stage 1.

--- assets/levels/Level1Spec.c
+++ assets/levels/Level1Spec.c
@@ -20,7 +20,7 @@
 //												DECLARATIONS
 //---------------------------------------------------------------------------------------------------------
 
-extern StageEntryPointROMSpec LEVEL_1_STAGE_4_MAIN_EP;
+extern StageEntryPointROMSpec LEVEL_1_STAGE_1_MAIN_EP;
 
 
 //---------------------------------------------------------------------------------------------------------
@@ -30,7 +30,7 @@ extern StageEntryPointROMSpec LEVEL_1_STAGE_4_MAIN_EP;
 PlatformerLevelROMSpec LEVEL_1_LV =
 {
 	// starting entry point
-	(StageEntryPointSpec*)&LEVEL_1_STAGE_4_MAIN_EP,
+	(StageEntryPointSpec*)&LEVEL_1_STAGE_1_MAIN_EP,
 
 	// id
 	1,
